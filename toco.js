b7dc83f1e1bd95f5667702480c4d2939b7e0f5d4
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-03-14 20:03:32 +0330

    made the structure work
    header, main and footer are staying where they should on desktop and mobile

---
+++ toco.js
@@ -0,0 +1,13 @@
+class Task {
+  constructor(title, date=new Date(), status=false){
+    this.title = title;
+    this.date = date;
+    this.status = status;
+  }
+  statusReport() {
+    return this.status? 'the task is done':'task is not done';
+  }
+}
+
+
+
