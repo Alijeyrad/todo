e58b3dd266fb964fa00f6bb773bd0f66faa5fc46
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-03-15 22:57:13 +0330

    add task works now

--- todo.js
+++ todo.js
@@ -1,10 +1,73 @@
-class Task {
-  constructor(title, date=new Date(), status=false){
-    this.title = title;
-    this.date = date;
-    this.status = status;
+var idMaker = function() {
+  let x = Date.now().toString(36);
+  let y = Math.random().toString(36).substr(2);
+  return x + y;
+}
+
+
+var taskManager = function() {
+  const tasks = [
+    {
+      id: 1,
+      title: 'Demo Task',
+      date: new Date(),
+      status: true,
+      element: function(){
+        return `<div id="task">
+          <div class="form-check">
+            <input class="form-check-input shadow-none" type="checkbox" id="${this.id}">
+            <label class="form-check-label" for="${this.id}">
+              ${this.title}
+            </label>
+          </div>
+        </div>`
+          }
+    }
+  ];
+  
+  var startPage = function() {
+    let innerHtml = '';
+    for (item of tasks) {
+      if (item.status) {
+        innerHtml += item.element();
+      }
+    }
+    document.getElementsByClassName('wrapper')[0].innerHTML = innerHtml;
   }
-  statusReport() {
-    return this.status? 'the task is done':'task is not done';
+
+  return {
+    addTask: function(event) {
+      event.preventDefault();
+      let text = event.path[2][0].value;
+      let date = new Date();
+      let status = true;
+      let id = idMaker();
+      tasks.push(
+        {
+          id: id,
+          title: text,
+          date: date,
+          status: status,
+          element: function(){
+            return `<div id="task">
+              <div class="form-check">
+                <input class="form-check-input shadow-none" type="checkbox" id="${this.id}">
+                <label class="form-check-label" for="${this.id}">
+                  ${this.title}
+                </label>
+              </div>
+            </div>`
+          }
+        }
+      )
+      console.log(tasks)
+      startPage();
+    },
+
+    startApp: function() {
+      return startPage();
+    }
   }
 }
+
+let x = taskManager();
